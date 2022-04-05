7e7e12320bcbe70356c788a7482b38c7fa7353eb
asawadichai <sawadichai.andrew@gmail.com>
2022-04-04 20:50:26 -0700

    Complete React Components.

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-// TODO
+//import React from 'react';
+
+class GroceryListItem extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        done: false
+      };
+    }
+    onMouseOver() {
+        this.setState({
+            done: !this.state.done
+          });
+        }
+        render() {
+          var style = {
+              fontWeight: this.state.done ? 'bold' : 'normal'
+            };
+
+            return(
+              <li style={style} onMouseOver={this.onMouseOver.bind(this)}>{this.props.groceryItem}</li>
+              );
+            }
+          };
+
+  var GroceryList = (props) => (
+    <ul>
+      {props.groceryItems.map(groceryItem =>
+        <GroceryListItem groceryItem={groceryItem} />
+      )}
+    </ul>
+);
+
+ReactDOM.render(<GroceryList groceryItems={['milk', 'eggs', 'coffee']}/>, document.getElementById("app"));
